542f61c72449bbabee4a2882b6b8753755d85b78
Warek <alexandrudobrojan@gmail.com>
2020-08-25 17:55:49 +0300

    Updated slider, fixed dropdown info, added team content

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-// Vanilla: facts
-// jQuery: open menu, comments slider
-//
-//
-
-! function facts() {  
-
-   // let bodyRect = document.body.getBoundingClientRect(),
-   //    elemRect = document.querySelector('.facts').getBoundingClientRect(),
-   //    offset = elemRect.top - bodyRect.top;
-   //    console.log(offset);
+"use strict";
+!function facts() {
 
    window.addEventListener('scroll', count);
 
@@ -56,42 +47,72 @@
 
 }();
 
-$(".about-job .header").click(function (e) {
-   $(this).find(".arrow").css("transform", "rotate(0deg)"); // Rotate arrow of focused cell
 
-   if ($(this).parent().css("height") === "60px") { // If focused cell is compressed
-      $(".about-job .cell").get().forEach(elem => { // Iterate cells to compress/expand
+// Dropdown Info (3 cells)
+let isDropdownBlocked = false;
+!function dropDown() {
 
-         if ($(elem).css("height") === "240px") { // If cell is expanded
-            $(elem).css("height", "60px")
-               .find(".arrow").css("transform", "rotate(180deg)")
-               .end()
-               .find("header").css("border", "none");
+   $(".about-job .header").click(function (e) {
+      if (isDropdownBlocked === false) {
+
+         isDropdownBlocked = true;
+
+         $(this).find(".arrow").css("transform", "rotate(0deg)"); // Rotate arrow of focused cell
+
+         if ($(this).parent().css("height") === "60px") { // If focused cell is compressed
+            $(".about-job .cell").get().forEach(elem => { // Iterate cells to compress/expand
+
+               if ($(elem).css("height") === "240px") { // If cell is expanded
+                  $(elem).css("height", "60px")
+                     .find(".arrow").css("transform", "rotate(180deg)")
+                     .end()
+                     .find("header").css("border", "none");
+               }
+            });
          }
-      });
-   }
 
-   $(this).css("border-bottom", "1px solid #e5e5e5")
-      .parent().css("height", "240px");
-});
+         $(this).css("border-bottom", "1px solid #e5e5e5")
+            .parent().css("height", "240px");
+
+         setTimeout(() => {
+            isDropdownBlocked = false; // Unlock dropdown
+         }, 300);
+      }
+   });
+}();
 
 
-! function slider() {
+!function slider() {
 
    let comments = $(".comment-section-wrapper ul.comments"), // comments slider
-      offset = 0;
+      offset = -1140;
 
-   $(".comment-section-wrapper img.arrow-right").click(function(e) {
+   $(".comment-section-wrapper img.arrow-right").click(function (e) {
       if (offset > -1140 * 4) {
+
          offset -= 1140;
          comments.css("left", offset);
+         
+      } else if (offset === -1440 * 3) {
+
+         offset = 0;
+         comments.css("left", offset);
+         
       }
    });
 
-   $(".comment-section-wrapper img.arrow-left").click(function(e) {
-      if (offset < 0 && offset != 1140 * 4) {
+   $(".comment-section-wrapper img.arrow-left").click(function (e) {
+      if (offset < 0 && offset < 1140 * 4) {
+
          offset += 1140;
          comments.css("left", offset);
+         console.log(offset)
+
+      } else if (offset >= 0) {
+
+         offset = -1140 * 3;
+         comments.css("left", offset);
+
       }
    });
 

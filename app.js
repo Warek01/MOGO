09cedddd2bc0431eca12e13c100876e3cd2bb7e6
Warek <alexandrudobrojan@gmail.com>
2020-08-27 00:12:36 +0300

    app.js log improvements in interface

--- app.js
+++ app.js
@@ -10,10 +10,21 @@ app.use(log, express.static(__dirname));
 app.listen(5554);
 
 function log(req, res, next) {
-   console.log(chalk`{white.bold.bgBlack Got request for} {blue.underline ${req.url}}`,
-      chalk`{white.bold.bgBlack from} {green.underline ${req.headers['x-forwarded-for'] || req.connection.remoteAddress}}`,
-      chalk`{bgRed.white :${req.connection.remotePort}}`,
-      chalk`| {bgWhite.black ${req.connection.remoteFamily}}`
+
+   if (req.url === "/") {
+      console.log(
+         chalk.greenBright("---------------------------------------------------------------------------\n") +
+         chalk.hex("#ff6b81").bold("New connection from") + " " +
+         chalk.yellow.italic.underline((req.headers['x-forwarded-for'] || req.connection.remoteAddress).match(/([\d\.]+)/)[0]) + ":" +
+         chalk.redBright(req.connection.remotePort) + " " +
+         chalk.bgWhite.black(req.connection.remoteFamily) +
+         "\non " + 
+         chalk.hex("#34ace0")(`${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`)
+      );
+   }
+
+   console.log(chalk`{white.bold.dim Requested} {blueBright.underline ${req.url}}`,
+      
    );
    next();
 }

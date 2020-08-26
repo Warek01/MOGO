01f3f580980605ee76e9b6285a992a9c8857db16
Warek <alexandrudobrojan@gmail.com>
2020-08-26 12:06:33 +0300

    Counter on scroll fixed, minor fixes

--- script.js
+++ script.js
@@ -9,32 +9,31 @@
       if (document.documentElement.scrollTop >= 865 && document.documentElement.scrollTop <= 1560) {
          for (let fact of facts) {
 
-            let limit = fact.textContent;
+            let limit = parseInt(fact.textContent);
             let i = 0;
 
             fact.textContent = 0;
 
             setTimeout(() => {
 
-               console.log(fact, limit)
-
                if (limit > 50) {
                   let interval = setInterval(() => {
                      fact.textContent = i;
+                     if (i === limit) clearInterval(interval);
                      i++;
-                     if (i - 1 === limit) clearInterval(interval);
+                     console.log(i, limit, limit === i)
                   }, 18);
                } else if (limit === 42) {
                   let interval = setInterval(() => {
                      fact.textContent = i;
+                     if (i === limit) clearInterval(interval);
                      i++;
-                     if (i - 1 === limit) clearInterval(interval);
                   }, 75);
                } else {
                   let interval = setInterval(() => {
                      fact.textContent = i;
+                     if (i === limit) clearInterval(interval);
                      i++;
-                     if (i - 1 === limit) clearInterval(interval);
                   }, 100);
                }
             }, 750);
@@ -84,20 +83,21 @@ let isDropdownBlocked = false;
 
 !function slider() {
 
-   let comments = $(".comment-section-wrapper ul.comments"), // comments slider
-      offset = -1140;
+   let comments = $(".comment-section-wrapper ul.comments"); // comments slider
+   let offset = -1140;
 
+   // Slide width: 1140px
    $(".comment-section-wrapper img.arrow-right").click(function (e) {
       if (offset > -1140 * 4) {
 
          offset -= 1140;
          comments.css("left", offset);
-         
+
       } else if (offset === -1140 * 4) {
 
          offset = 0;
          comments.css("left", offset);
-         
+
       }
    });
 
